d80eaeb92119f31cf458bfba952b8ba74d59b0b5
Melissa <mvazquez14012@plataforma-utslp.net>
2024-02-23 20:30:59 -0600

    utslp230224

---
+++ prueba.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+fecha=`date +%Y/%m/%d/%T`
+#fecha actual
+
+mkdir -p nuevo
+#Creamos carpeta
+
+touch nuevo/archivo
+#
+
+echo " $fecha Script1" >> nuevo/archivo
+
+cat nuevo/archivo
+
+if [ -e "$archivo" ]; then
+    echo "El archivo $archivo existe."
+else
+    echo "El archivo $archivo no existe."
+fi
+
