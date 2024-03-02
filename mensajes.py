1cbc87257c215816139947076c629f1d3ae9b1d1
mvq2199 <148651367+mvq2199@users.noreply.github.com>
2024-03-01 22:55:40 -0600

    python01022024

---
+++ mensajes.py
@@ -0,0 +1,10 @@
+import pywhatkit 
+
+try:
+    #pywhatkit.sendwhatmsg("+524445758910", "Hola Melissa /n", 8,34)
+    pywhatkit.sendwhatmsg_instantly("+524445758910", "Hola Melissa")
+    print("Operación exitosa")
+except:
+    print("Error")    
+
+
